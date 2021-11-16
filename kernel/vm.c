c2482e0180ac4ba47e101e89e997cbe88267ef30
Sheen Lin <563894087@qq.com>
2021-11-16 20:28:21 +0800

    pgtl

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void dfs(pagetable_t pagetable, int level) {
+  for (int i = 0; i < 512; ++i) {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V)) {
+      uint64 child = PTE2PA(pte);
+      for (int i = 0; i <= level; ++i) {
+        printf("..");
+        if (i < level) {
+          printf(" ");
+        }
+      }
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      if (level <= 1) {
+        dfs((pagetable_t)child, level + 1);
+      }
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  dfs(pagetable, 0);
+}
